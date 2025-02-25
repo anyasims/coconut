d369d5fd7527f8ac784f3d362b1ccf09eeaa407e
anyasims <anya.sims@jesus.ox.ac.uk>
2025-02-25 15:24:34 +0000

    dev

--- setup.sh
+++ setup.sh
@@ -32,3 +32,12 @@ cd /home/anya/Documents/llm_tiny_ideas/coconut-outer/coconut
 export PYTHONPATH=$PYTHONPATH:/home/anya/Documents/llm_tiny_ideas/coconut-outer/coconut
 # ^Call with:
 # source /home/anya/Documents/llm_tiny_ideas/coconut-outer/coconut/setup.sh
+
+
+## For running:
+### GSM8K
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nnodes 1 --nproc_per_node 4 run.py args/gsm_cot.yaml # stage 0
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nnodes 1 --nproc_per_node 4 run.py args/gsm_coconut.yaml # coconut
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nnodes 1 --nproc_per_node 4 run.py args/gsm_coconut_eval.yaml # eval
+### ProsQA
+# CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nnodes 1 --nproc_per_node 4 run.py args/prosqa_coconut.yaml
